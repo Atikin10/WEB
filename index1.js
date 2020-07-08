69bd9db8f023b5789eecea835990724b1a9317bb
Atikin10 <57946400+Atikin10@users.noreply.github.com>
2020-07-09 02:58:02 +0300

    LET

---
+++ index1.js
@@ -0,0 +1,11 @@
+let a=10;
+let b=2;
+let s=a+b;
+let r=a-b;;
+let p=a*b;
+let d=a/b;
+console.log("Сумма="+s);
+console.log("Разность="+r);
+console.log("Произведение="+p);
+console.log("Частное="+d);
+
---
+++ index4.js
@@ -0,0 +1,6 @@
+let a=17;
+let b=10;
+const c=a-b;
+let d=7;
+let result=c+d;
+console.log(result);
